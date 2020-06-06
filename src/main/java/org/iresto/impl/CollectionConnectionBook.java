fc5d57c4cea0b0800e59cab69e8e57239670c268
User <nexx01@mail.ru>
2020-06-06 22:12:28 +0300

    Добавлен метод с тест данными

--- src/main/java/org/iresto/impl/CollectionConnectionBook.java
+++ src/main/java/org/iresto/impl/CollectionConnectionBook.java
@@ -1,10 +1,15 @@
 package org.iresto.impl;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.iresto.impl.interfaces.ConnectionBook;
 import org.iresto.object.AbstractClient;
+import org.iresto.object.impl.clientIiko.ClientIiko;
 
 public class CollectionConnectionBook implements ConnectionBook {
+
+    ObservableList<ClientIiko> clientIikoObservableList= FXCollections.observableArrayList();
 
     @Override
     public void add(AbstractClient client) {
@@ -20,4 +25,18 @@ public class CollectionConnectionBook implements ConnectionBook {
     public void delete(AbstractClient client) {
 
     }
+
+    public ObservableList<ClientIiko> getClientIikoObservableList() {
+        return clientIikoObservableList;
+    }
+
+    public void fillTestDataClientIiko(){
+        clientIikoObservableList.add(new ClientIiko(
+                "Мир Пиццы", "ИП Чеснокова", "Павлово,Гвардейцев 18"));
+        clientIikoObservableList.add(new ClientIiko(
+                "Мир Пиццы", "ИП Захарова", "Дзержинск,Циолковского 19"));
+        clientIikoObservableList.add(new ClientIiko(
+                "Самурай", "ООО Венусто", "Нижний Новгород, Белинского 61"));
+    }
+
 }
