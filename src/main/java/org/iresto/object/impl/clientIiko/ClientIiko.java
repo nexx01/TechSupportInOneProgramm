1e3ce4952f6ba236e3ead1e6baf327530659dc32
User <nexx01@mail.ru>
2020-06-06 22:01:17 +0300

    Удален Деление клиентов айко на главный и нет. Терерь только один клиент

---
+++ src/main/java/org/iresto/object/impl/clientIiko/ClientIiko.java
@@ -0,0 +1,11 @@
+package org.iresto.object.impl.clientIiko;
+
+
+import org.iresto.object.AbstractClientIIKO;
+
+public class ClientIiko extends AbstractClientIIKO {
+
+    public ClientIiko(String brand, String legalEntity, String statusOfSupport) {
+        super(brand, legalEntity, statusOfSupport);
+    }
+}
