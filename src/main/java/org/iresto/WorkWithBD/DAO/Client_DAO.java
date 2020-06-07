81da644f692140a7963a9895df8028e12e9c6058
User <nexx01@mail.ru>
2020-06-07 15:46:51 +0300

    create Client_DAO.java create method findAllClient

---
+++ src/main/java/org/iresto/WorkWithBD/DAO/Client_DAO.java
@@ -0,0 +1,32 @@
+package org.iresto.WorkWithBD.DAO;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.iresto.WorkWithBD.ConnectorDB;
+import org.iresto.object.impl.clientIiko.ClientIiko;
+
+import java.sql.*;
+
+public class Client_DAO {
+    public ObservableList<ClientIiko>  findAllClient(){
+        ObservableList<ClientIiko> clientsList=FXCollections.observableArrayList();
+        ClientIiko clientIiko=null;
+        String SELECT_QUERY="SELECT * FROM clientiiko";
+        try(Connection connection= ConnectorDB.getConnecton();
+            Statement statement=connection.createStatement()){
+            ResultSet resultSet=statement.executeQuery(SELECT_QUERY);
+            while(resultSet.next()){
+                int id =resultSet.getInt(1);
+                String brand=resultSet.getString(2);
+                String legalEntity=resultSet.getString(3);
+                String address = resultSet.getString(4);
+                String kindOfLicense = resultSet.getString(5);
+                String statusOfSupport = resultSet.getString(6);
+            }
+            return clientsList;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+}
