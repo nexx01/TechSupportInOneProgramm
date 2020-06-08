f64f63b18c8f76e417965075c098344e70340f2f
User <nexx01@mail.ru>
2020-06-08 12:54:34 +0300

    метод для заполнения  для таблицы с подключениями

--- src/main/java/org/iresto/SecondaryController.java
+++ src/main/java/org/iresto/SecondaryController.java
@@ -3,8 +3,16 @@ package org.iresto;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.iresto.WorkWithBD.DAO.ConnectData_DAO;
+import org.iresto.object.impl.WorkComputer.WorkComputer;
 import org.iresto.object.impl.clientIiko.ClientIiko;
 
 
@@ -14,8 +22,15 @@ public class SecondaryController implements Initializable {
     public Label lblBrand;
     public Label lblAddress;
     public Label lblStatusOfSupport;
-    private ClientIiko clientIiko;
+    public TableView tableOfConnectData;
+    public TableColumn columnTypePC;
+    public TableColumn columnAmmyAdmin;
+    public TableColumn columnAnydesk;
 
+
+    private ClientIiko clientIiko;
+   private ObservableList<WorkComputer> workComputersImpl=FXCollections.observableArrayList();
+   private ConnectData_DAO connectData_dao=new ConnectData_DAO();
 
     private ResourceBundle resourceBundle;
     @Override
@@ -32,6 +47,17 @@ public class SecondaryController implements Initializable {
         lblLegalEntity.setText(clientIiko.getLegalEntity());
         lblAddress.setText(clientIiko.getAddress());
         lblStatusOfSupport.setText(clientIiko.getStatusOfSupport());
+
+
+        columnAmmyAdmin.setCellValueFactory(new PropertyValueFactory<WorkComputer,String>("IDAmmyAdmin"));
+        columnTypePC.setCellValueFactory(new PropertyValueFactory<WorkComputer,String>("typePC"));
+        columnAnydesk.setCellValueFactory(new PropertyValueFactory<WorkComputer,String>("IDAnyDesk"));
+        fillData(clientIiko.getClientId());
+    }
+
+    private void fillData(int clientId){
+        workComputersImpl=connectData_dao.findConnectDataByClientId(clientId);// выкачиваем данные и раззрываем
+        tableOfConnectData.setItems(workComputersImpl);//заполняем таблицу интерфейса
     }
 
     /*   @FXML
