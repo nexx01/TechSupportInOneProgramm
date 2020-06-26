332c3e685c3628565a691367c28972823e4fac2e
Alexey Ashutov <i.ashutov@yandex.ru>
2020-06-26 18:06:30 +0300

    Все поля String переделаны в SimpleStringProperty для работы с таблицей

--- src/main/java/org/iresto/object/AbstractWorkComputer.java
+++ src/main/java/org/iresto/object/AbstractWorkComputer.java
@@ -1,80 +1,89 @@
 package org.iresto.object;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public abstract class AbstractWorkComputer {
 
-    private String typePC;
+    //private String typePC;
+    private SimpleStringProperty typePC =new SimpleStringProperty();
+
     /* Тип рабочей станции, а именно ChainServer,Front
      * RMSServer, Kitchen, Arrivals и т.д.*/
-    private String IDAmmyAdmin;
+   // private String IDAmmyAdmin;
+    private SimpleStringProperty IDAmmyAdmin =new SimpleStringProperty();
     /* ID подключение по AmmyAdmin */
-    private String IDAnyDesk;
+    //private String IDAnyDesk;
+    private SimpleStringProperty IDAnyDesk =new SimpleStringProperty();
     /* ID подключение по AnyDesk */
-    private String PswAmmyAdmin;
+    //private String PswAmmyAdmin;
+    private SimpleStringProperty PswAmmyAdmin =new SimpleStringProperty();
     /* Пароль подключение по AmmyAdmin */
-    private String PswAnyDesk;
+   // private String PswAnyDesk;
+    private SimpleStringProperty PswAnyDesk =new SimpleStringProperty();
     /* Пароль подключение по AnyDesk */
-    private String hostServer;
+    //private String hostServer;
+    private SimpleStringProperty hostServer =new SimpleStringProperty();
     /*хост или внешний ip сервера iiko. Вместе с портом
      * например http:\\demo-stend-1.iiko.it:8080
      **/
 
     public AbstractWorkComputer(String typePC, String IDAmmyAdmin, String IDAnyDesk, String pswAmmyAdmin, String pswAnyDesk) {
-        this.typePC = typePC;
-        this.IDAmmyAdmin = IDAmmyAdmin;
-        this.IDAnyDesk = IDAnyDesk;
-        PswAmmyAdmin = pswAmmyAdmin;
-        PswAnyDesk = pswAnyDesk;
+        this.typePC = new SimpleStringProperty(typePC);
+        this.IDAmmyAdmin = new SimpleStringProperty(IDAmmyAdmin);
+        this.IDAnyDesk = new SimpleStringProperty(IDAnyDesk);
+        PswAmmyAdmin = new SimpleStringProperty(pswAmmyAdmin);
+        PswAnyDesk = new SimpleStringProperty(pswAnyDesk);
     }
 
     public AbstractWorkComputer(String typePC) {
-        this.typePC = typePC;
+        this.typePC.set(typePC);
     }
 
     public void setHostServer(String hostServer) {
-        this.hostServer = hostServer;
+        this.hostServer.set(hostServer);
     }
 
     public String getTypePC() {
-        return typePC;
+        return typePC.get();
     }
 
     public String getIDAmmyAdmin() {
-        return IDAmmyAdmin;
+        return IDAmmyAdmin.get();
     }
 
     public String getIDAnyDesk() {
-        return IDAnyDesk;
+        return IDAnyDesk.get();
     }
 
     public String getPswAmmyAdmin() {
-        return PswAmmyAdmin;
+        return PswAmmyAdmin.get();
     }
 
     public String getPswAnyDesk() {
-        return PswAnyDesk;
+        return PswAnyDesk.get();
     }
 
     public String getHostServer() {
-        return hostServer;
+        return hostServer.get();
     }
 
     public void setTypePC(String typePC) {
-        this.typePC = typePC;
+        this.typePC.set(typePC);
     }
 
     public void setIDAmmyAdmin(String IDAmmyAdmin) {
-        this.IDAmmyAdmin = IDAmmyAdmin;
+        this.IDAmmyAdmin.set(IDAmmyAdmin);
     }
 
     public void setIDAnyDesk(String IDAnyDesk) {
-        this.IDAnyDesk = IDAnyDesk;
+        this.IDAnyDesk.set(IDAnyDesk);
     }
 
     public void setPswAmmyAdmin(String pswAmmyAdmin) {
-        PswAmmyAdmin = pswAmmyAdmin;
+        this.PswAmmyAdmin.set(pswAmmyAdmin);
     }
 
     public void setPswAnyDesk(String pswAnyDesk) {
-        PswAnyDesk = pswAnyDesk;
+        this.PswAnyDesk.set(pswAnyDesk);
     }
 }
